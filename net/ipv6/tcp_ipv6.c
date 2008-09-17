b1761fadfb3f98f445a34a92b86aa46798b37feb
Sébastien Barré <sebastien.barre@uclouvain.be>
2008-09-17 19:00:29 +0200

    Added a notification handler for path change event
    All sockets with retransmission timer armed have that timer reset,
    so that a packet can go out immediately

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -2232,5 +2232,5 @@ void tcpv6_exit(void)
 	unregister_netevent_notifier(&nb);
 	unregister_pernet_subsys(&tcpv6_net_ops);
 	inet6_unregister_protosw(&tcpv6_protosw);
-	inet6_del_protocol(&tcpv6_protocol, IPPROTO_TCP);	
+	inet6_del_protocol(&tcpv6_protocol, IPPROTO_TCP);
 }
